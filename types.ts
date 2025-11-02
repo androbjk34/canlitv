7e757879c74adbf1f7e19f68c347a1d633c7b177
androbjk34 <androbjk34@gmail.com>
2025-11-02 20:35:13 +0300

    Add files via upload

---
+++ types.ts
@@ -0,0 +1,17 @@
+export interface EpgEvent {
+  title: string;
+  startTime: Date;
+  endTime: Date;
+  synopsis?: string;
+  cast?: string[];
+}
+
+export interface Channel {
+  name: string;
+  logo: string | null;
+  url: string;
+  group: string | null;
+  tvgId: string | null;
+  tvgName: string | null;
+  currentEpg?: EpgEvent | null;
+}
